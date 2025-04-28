0db797d03d38d75bc449e262c5db92087204bad5
Josephineoe <josephineodus@gmail.com>
2025-04-28 14:08:02 -0400

    yooo

--- homework_7/question5.py
+++ homework_7/question5.py
@@ -5,12 +5,14 @@ GPIO.setmode(GPIO.BCM)
 Ping_pin = 23
 LED_pin = 22
 
+GPIO.setup(LED_pin, GPIO.OUT)
+
 #Open file to write and save distance data
 file = open("rdistancelogs.txt", "w")
 #While loop to monitor distance
+
 while(1):
     GPIO.setup(Ping_pin, GPIO.OUT)
-    GPIO.setup(LED_pin, GPIO.OUT)
     #First set Ping_pin to output to send signal
 
     #cleanup output to send a clean high signal
@@ -21,28 +23,32 @@ while(1):
     time.sleep(0.000005)
     #cleanup output pin
     GPIO.output(Ping_pin, 0)
+
     #Make Ping_pin input pin to monitor Ping sensor SIG pin
     GPIO.setup(Ping_pin, GPIO.IN)
     # starttime=time.time()
+
     while GPIO.input(Ping_pin)==0:
         starttime=time.time()
     while GPIO.input(Ping_pin)==1:
         endtime=time.time()
-         #Duration that the high pulse lasts 
+
+    #Duration that the high pulse lasts 
     duration=endtime-starttime
     #Round trip distance from Ping2Object and Object2Ping
-    distance=duration*34000
-    #Ping2Object distance
-    distance=distance/2
-    print(distance)
-    time.sleep(2)
+    distance=(duration*34000)/2 #cm, Ping2Object distance
+    
+    print(distance, "cm")
     file.write(str(distance)+"\n")
-file.close()
+    file.flush() #writes to file
 
-while True: 
-    if distance >= 20:
+    if distance > 20:
         GPIO.output(LED_pin, 1)
-        time.sleep(0.5)
+        time.sleep(0.1)
         GPIO.output(LED_pin, 0)
+        time.sleep(0.1)
     else:
         GPIO.output(LED_pin, 0)
+    
+    time.sleep(2)
+file.close()
